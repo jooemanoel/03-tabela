1c6fd1b59984dcb7c978a9c3ecdd358d2e14c6dc
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-07-31 11:22:10 -0300

    Ordenação decrescente

--- js/Lista.js
+++ js/Lista.js
@@ -3,8 +3,13 @@ import { Item } from "./Item.js";
 export class Lista {
     constructor(nomeDaLista) {
         this.itens = JSON.parse(localStorage.getItem(nomeDaLista)) || [];
+        this.ordenar();
+    }
+    ordenar() {
+        this.itens = this.itens.sort((i1, i2) => i2.qt - i1.qt);
     }
     salvar() {
+        this.ordenar();
         localStorage.setItem('itens', JSON.stringify(this.itens));
     }
     novoItem(id, md) {
