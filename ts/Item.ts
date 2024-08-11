0311a14bf0514d36b0801384003d477fce087562
jooemanoel <jooemanoel@gmail.com>
2024-08-11 20:19:44 -0300

    Mudei item para interface

--- ts/Item.ts
+++ ts/Item.ts
@@ -1,10 +1,5 @@
-export class Item{
+export interface Item {
     id: string;
     qt: number;
     md: string;
-    constructor(id: string, qt: number, md: string){
-        this.id = id;
-        this.qt = qt;
-        this.md = md;
-    }
 }
