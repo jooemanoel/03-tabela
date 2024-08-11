aa5225d9aa3c256134f23552aee24da14855a5d9
jooemanoel <jooemanoel@gmail.com>
2024-08-11 20:21:09 -0300

    Revert "Mudei item para interface"
    This reverts commit 0311a14bf0514d36b0801384003d477fce087562.

--- ts/Item.ts
+++ ts/Item.ts
@@ -1,5 +1,10 @@
-export interface Item {
+export class Item{
     id: string;
     qt: number;
     md: string;
+    constructor(id: string, qt: number, md: string){
+        this.id = id;
+        this.qt = qt;
+        this.md = md;
+    }
 }
